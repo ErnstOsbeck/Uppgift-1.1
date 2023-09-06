2ae46f4ebb5893b854ae0dfd66098c56d00350ec
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-09-06 10:22:14 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace uppgift_1._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig att skriva ut tecken som \" och \\.");
+        }
+    }
+}
